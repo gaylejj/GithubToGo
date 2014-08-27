cc20e8273644e855aacf165fd769898cb26f99e2
Jeff Gayle <jeff.j.gayle@gmail.com>
2014-08-26 17:38:46 -0700

    updated .gitignore

---
+++ GithubToGo2/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  GithubToGo2
+//
+//  Created by Jeff Gayle on 8/26/14.
+//  Copyright (c) 2014 Jeff Gayle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+@end
---
+++ GithubToGo2/Constants.m
@@ -0,0 +1,13 @@
+//
+//  Constants.m
+//  GithubToGo2
+//
+//  Created by Jeff Gayle on 8/26/14.
+//  Copyright (c) 2014 Jeff Gayle. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+@end
